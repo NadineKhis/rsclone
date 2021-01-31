ab9f71061b992b6bce6b052254a766362a85e8cf
nadinekhis <torontino123@gmail.com>
2021-01-31 19:39:44 +0300

    fix: fix props validation

--- src/components/browseBody/browseBody.js
+++ src/components/browseBody/browseBody.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './browseBody.css'
 import { FilmCategoryItem } from '../filmCategoryItem/filmCategoryItem'
 import { UserSettings } from '../userSettings/userSettings'
+import PropTypes from 'prop-types';
 
 export function BrowseBody(props) {
     const browseStyle = {
@@ -22,7 +23,7 @@ export function BrowseBody(props) {
                 <div className='browseBody_wrapper'>
                     <div className='current_film_describe' style={browseStyle}>
                         <h2>Watch Joker now</h2>
-                        <p>Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks - the one he paints for his job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him.</p>
+                        <p>Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks - the one he paints for his job as a clown, and the guise he projects in a futile attempt to feel like he&apos;s part of the world around him.</p>
                         <button>Play</button>
                     </div>
                     <div className='main_wrapper'>
@@ -31,4 +32,9 @@ export function BrowseBody(props) {
                 </div>
             )
     }
+}
+
+BrowseBody.propTypes = {
+    children: PropTypes.node,
+    bodyType: PropTypes.node
 }
